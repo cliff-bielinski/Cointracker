9f001c944caff5698ab2138d1bc8fea7d9d322ed
cliff-bielinski <bielinski.cm@gmail.com>
2021-08-14 16:19:07 -0700

    folder structure updated and renamed files

---
+++ cointracker/query.py
@@ -0,0 +1,21 @@
+import requests
+from pprint import pprint
+
+def request_data(data_type, coin, subreddit, timeframe, size):
+    """
+    Retrieves query data from pushshift API
+    data_type can be 'comment' or 'submission'
+    timeframe in 'value+s/m/h/d' (second, minute, hour, day) e.g. '30d' for past 30 days
+    returns query results in JSON
+
+    https://github.com/pushshift/api
+    """
+
+    search_params = {'q': coin, 'subreddit': subreddit, 'after': timeframe, 'size': size}
+    url = "https://api.pushshift.io/reddit/search/{}/".format(data_type)
+
+    data = requests.get(url, params=search_params)
+
+    return data.json()
+
+pprint(request_data('submission', 'BTC', 'CryptoCurrency', '30d', '1'))
