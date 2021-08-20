bb11c1784ed547932a0f526f614bd562a535b5c4
Cliff Bielinski <bielinski.cm@gmail.com>
2021-08-20 13:36:37 -0700

    clarified method docstrings

--- cointracker/sanitize.py
+++ cointracker/sanitize.py
@@ -1,16 +1,16 @@
 import re
 
 def remove_punctuation(comment):
-    """returns string with non-alphanumeric or whitespace characters removed"""
+    """takes string and returns it with non-alphanumeric or whitespace characters removed"""
     return re.sub(r'[^\w\s]', '', comment)
 
 def remove_whitespace(comment):
-    """returns string with excess whitespace removed"""
+    """takes string and returns it with excess whitespace removed"""
     comment = re.sub(r'\s+', ' ', comment)
     return comment.strip()
 
 def sanitize_text(comment):
-    """takes string as input and returns sanitized version without punctuation, whitepace or capitalization"""
+    """takes string and returns sanitized version without punctuation, whitepace or capitalization"""
     comment = comment.lower()
     comment = remove_punctuation(comment)
     comment = remove_whitespace(comment)
