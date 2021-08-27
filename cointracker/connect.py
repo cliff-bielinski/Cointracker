5c10167392821594db7e96b388229d956c09cd78
Cliff Bielinski <bielinski.cm@gmail.com>
2021-08-26 23:07:08 -0700

    sql queries to insert rows now functional

--- cointracker/connect.py
+++ cointracker/connect.py
@@ -1,24 +1,18 @@
 import psycopg2
 from config.config import config
 
-def insert_row(comment_dict, subreddit_dict):
+def insert_row(table_name, data):
     """
-    connect to the database server and inserts prepared comment as row in comments table for db
+    connect to the database server and inserts data as row in table for db
     Args:
-        comment_dict (dictionary): a map of comment metadata to store as a row in the database
+        table_name(str): the table having the row of data inserted into
+        data (dictionary): a map of data to store as a row in the database
     
     Returns:
         a string confirming successful upload into db
     """
-    def commit_row(table_name, query, data):
-        """
-        executes SQL query and commits it to database
-
-        Args:
-            table_name (str) - name of table row to be inserted in
-            query (str) - SQL query to be executed by db
-            data (dict) - map of columns to data values
-        """
+    def commit_row(query):
+        """executes SQL query and commits it to database"""
         cursor.execute(query, data)
         connection.commit()
         count = cursor.rowcount
@@ -31,6 +25,7 @@ def insert_row(comment_dict, subreddit_dict):
         connection = psycopg2.connect(**db_params)
         cursor = connection.cursor()
 
+        # SQL query for insertion into subreddits table
         subreddit_query = """
             INSERT INTO subreddits (subreddit_id, subreddit_name)
             VALUES (%(subreddit_id)s, %(subreddit_name)s)
@@ -38,15 +33,41 @@ def insert_row(comment_dict, subreddit_dict):
             DO NOTHING;
             """
 
-        comment_query = """
-            INSERT INTO comments (comment_id, comment_time, parent_id, score, body, pulled_at, post_id, author_id, coin_id)
-            VALUES (%(id)s, %(time)s, %(parent)s, %(score)s, %(body)s, %(pulled)s, %(post)s, %(author)s, %(coin)s);
+        # SQL query for insertion into authors table
+        author_query = """
+            INSERT INTO authors (author_id, author_name)
+            VALUES (%(author_id)s, %(author_name)s)
+            ON CONFLICT (author_id)
+            DO NOTHING;
+            """
+
+        # SQL query for insertion into submissions table
+        submission_query = """
+            INSERT INTO submissions (post_id, post_name, author_id, coin_id, subreddit_id, submission_time)
+            VALUES (%(id)s, %(name)s, %(author)s, %(coin)s, %(subreddit_id)s, %(time)s)
+            ON CONFLICT (post_id)
+            DO NOTHING;
             """
         
-        commit_row('subreddits', subreddit_query, comment_dict)
+        # SQL query for insertion into comments table
+        comment_query = """
+            INSERT INTO comments (comment_id, comment_time, parent_id, score, body, pulled_at, post_id, author_id, coin_id)
+            VALUES (%(id)s, %(time)s, %(parent)s, %(score)s, %(body)s, %(pulled)s, %(post)s, %(author)s, %(coin)s)
+            ON CONFLICT (comment_id)
+            DO NOTHING;
+            """
+
+        if table_name == 'subreddits':
+            commit_row(subreddit_query)
+        elif table_name == 'authors':
+            commit_row(author_query)
+        elif table_name == 'submissions':
+            commit_row(submission_query)
+        elif table_name == 'comments':
+            commit_row(comment_query) 
 
     except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into comments table", error)
+        print(f"Failed to insert record into {table_name} table", error)
 
     finally:
         # closes connection
--- cointracker/querybot.py
+++ cointracker/querybot.py
@@ -29,42 +29,46 @@ def get_comments(reddit, subreddit, submission_num):
     return comments, submissions
 
 def prepare_comment(comment, sanitized_text, coin_matches=None):
+    """returns map of comment metadata"""
     comment_map = {
         'id':comment.id, 
         'time':datetime.fromtimestamp(comment.created_utc),
         'parent':comment.parent_id,
         'score':comment.score,
-        'body': sanitized_text,
+        'body':sanitized_text,
         'pulled':datetime.utcnow(),
-        'post':comment.link_id,
+        'post':comment.submission.id,
         'author':comment.author.id,
-        'coin': coin_matches,
+        'coin':coin_matches,
     }
     return comment_map
 
 def prepare_subreddit(comment):
+    """returns map of subreddit metadata"""
     subreddit_map = {
-        'subreddit_id': comment.subreddit_id,
-        'subreddit_name:': comment.subreddit.display_name
+        'subreddit_id':comment.subreddit.id,
+        'subreddit_name':comment.subreddit.display_name
     }
     return subreddit_map
 
 def prepare_author(comment):
+    """returns map of author metadata"""
     author_map = {
         'author_id':comment.author.id,
-        'author_name':comment.redditor.name
+        'author_name':comment.author.name
     }
     return author_map
 
 def prepare_submission(comment, sanitized_text, coin_matches=None):
+    """returns map of submission metadata"""
     submission_map = {
         'id':submission.id,
         'time':datetime.fromtimestamp(submission.created_utc),
         'name':submission.title,
         'author':comment.author.id,
         'body':sanitized_text,
-        'coin': coin_matches,
-        'subreddit_id': submission.subreddit.id 
+        'coin':coin_matches,
+        'subreddit_id':submission.subreddit.id 
     }
     return submission_map
 
@@ -73,15 +77,24 @@ if __name__ == '__main__':
     coin_wallet = wallet.Wallet(1000)
     comment_list, submission_list = get_comments(reddit, 'MUD', 1)
 
+    # prepares and inserts data from every submission returned from api call into the db
     for submission in submission_list:
         sanitized_submission = sanitize.sanitize_text(submission.selftext)
         submission_coins = list(coin_wallet.match_coins(sanitized_submission))
+        prepared_subreddit = prepare_subreddit(submission)
+        prepared_author = prepare_author(submission)
         prepared_submission = prepare_submission(submission, sanitized_submission, submission_coins)
+        print(prepared_subreddit)
+        connect.insert_row('subreddits', prepared_subreddit)
+        connect.insert_row('authors', prepared_author)
+        connect.insert_row('submissions', prepared_submission)
 
+    # prepares and inserts data from every comment returned from api call into the db
     for comment in comment_list:
         sanitized_comment = sanitize.sanitize_text(comment.body)
         mentioned_coins = list(coin_wallet.match_coins(sanitized_comment))
+        prepared_author = prepare_author(comment)
         prepared_comment = prepare_comment(comment, sanitized_comment, mentioned_coins)
-        # connect.insert_subreddit(comment.subreddit_id, comment.subreddit.display_name)
-        # connect.insert_row(prepared_comment, {'subreddit_id':'test_id3', 'subreddit_name':'ymp4life'})
+        connect.insert_row('authors', prepared_author)
+        connect.insert_row('comments', prepared_comment)
 
