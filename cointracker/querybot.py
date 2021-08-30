085e2cf99163e3e31cd4a52e6c350bc49ca06674
Cliff Bielinski <bielinski.cm@gmail.com>
2021-08-29 19:56:25 -0700

    added main() to querybot.py

--- cointracker/querybot.py
+++ cointracker/querybot.py
@@ -1,9 +1,12 @@
 import reddit
 import wallet
 
-if __name__ == '__main__':
+def main():
     session = reddit.Reddit('coinbot', 'praw.ini')
     coin_wallet = wallet.Wallet(1000)
     session.get_posts('daily', 'cryptocurrency', 2, coin_wallet, True)
     session.get_comment_ids_from_posts(True)
     session.get_comments(coin_wallet, True)
+
+if __name__ == '__main__':
+    main()
